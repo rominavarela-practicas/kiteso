9c5f343fd17cd64158238ace409d8375972b242d
rominavarela <rominavarela.mx@gmail.com>
2015-10-26 00:12:58 -0600

    session in encrypted cookie

--- src/main/java/mx/kiteso/KIteso/controller/SessionController.java
+++ src/main/java/mx/kiteso/KIteso/controller/SessionController.java
@@ -1,31 +1,77 @@
 package mx.kiteso.KIteso.controller;
 
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+
+import org.cryptonode.jncryptor.AES256JNCryptor;
 
 import mx.kiteso.KIteso.model.Session;
 
 import com.google.gson.Gson;
 
-public class SessionController {
+@SuppressWarnings("unused")
+public class SessionController{
+	
+	private static final byte[] keyValue =
+		      new byte[] {'O', 'p', 'e', 'n', 'S', 'a', 'y', 's', 'M', 'e', '1', '2', '3', '4', '5', '6'};
+	
 	public static void clearSession(HttpServletResponse res) {
 		res.addCookie( new Cookie("session", "") );
 	}
 	
 	public static void writeSession(Session session, HttpServletResponse res) {
 		Gson gson = new Gson();
-		Session cipheredSession = session.clone();
-		//TODO cipher content
-		//https://github.com/pcompton/cookie-sample/blob/master/src/main/java/com/issgovernance/cookiesample/domain/CookieEncryption.java
-		Cookie cookie = new Cookie("session", gson.toJson(cipheredSession));
+		String json = gson.toJson(session);
+		String cipheredSession = "";
+		
+		try
+		{
+			Key key = new SecretKeySpec(SessionController.keyValue, "AES");
+			Cipher c = Cipher.getInstance("AES");
+		    c.init(Cipher.ENCRYPT_MODE, key);
+		    byte[] ciphertext = c.doFinal(json.getBytes());
+		    Base64.Encoder encoder = Base64.getEncoder();
+		    cipheredSession = encoder.encodeToString(ciphertext);
+		} 
+		catch (Exception ex)
+		{
+			System.out.println(ex);
+		}
+		
+		Cookie cookie = new Cookie("session", cipheredSession);
 		res.addCookie(cookie);
 	}
 	
 	public static Session readSession(String sessionCookie){
-		Gson gson = new Gson();
-		Session cipheredSession = gson.fromJson(sessionCookie, Session.class);
-		Session session = cipheredSession.clone();
-		//TODO decipher content
+		Session session = new Session();
+		
+		if(!sessionCookie.isEmpty())
+			try
+			{
+				Key key = new SecretKeySpec(SessionController.keyValue, "AES");
+			    Cipher c = Cipher.getInstance("AES");
+			    c.init(Cipher.DECRYPT_MODE, key);
+			    Base64.Decoder decoder = Base64.getDecoder();
+			    byte[] decodedValue = decoder.decode(sessionCookie);
+			    byte[] decValue = c.doFinal(decodedValue);
+			    String decryptedValue = new String(decValue);
+			    
+			    Gson gson = new Gson();
+			    session = gson.fromJson(decryptedValue, Session.class);
+			}
+			catch(Exception ex)
+			{
+				System.out.println(ex);
+			}
+		
 		return session;
 	}
 }
